d2398fcead32e4965b652b54db1c30fcf008235f
Gebrin <74142003+Gebrin@users.noreply.github.com>
2026-09-03 18:29:25 +0530

    Create erp_px_cat.sql

---
+++ scripts/silver/erp_px_cat.sql
@@ -0,0 +1,11 @@
+
+truncate table silver.erp_px_cat_g1v2;
+
+insert into silver.erp_px_cat_g1v2(
+	id,
+	cat,
+	subcat,
+	maintenance
+)
+select id,cat,subcat,maintenance
+from bronze.erp_px_cat_g1v2
